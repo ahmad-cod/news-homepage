2ae5fdceee97121a08dfa8977cc073f37b4b6a10
ahmad-cod <ahmadadewale800@gmail.com>
2022-12-01 22:46:07 +0000

    Create responsive nav.

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-const toggle = document.getElementById('toggleMenu');
-const menuList = document.getElementById('menuList').style;
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
 
-menuList.display = 'none';
-
-toggle.addEventListener('click', () => {
-    if(!menuList.display){
-        menuList.display = 'none'
-    }
-    else {
-        menuList.display = ''
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+    
+    if (visibility === 'false') {
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
+        
+    } else {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
     }
 })
+
+
+// const toggle = document.getElementById('toggleMenu');
+// const menuList = document.getElementById('menuList').style;
+
+// menuList.display = 'none';
+
+// toggle.addEventListener('click', () => {
+//     if(!menuList.display){
+//         menuList.display = 'none'
+//     }
+//     else {
+//         menuList.display = ''
+//     }
+// })
