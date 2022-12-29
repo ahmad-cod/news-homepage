922863ada89ab620c7520f49b39652a426bbc486
ahmad-cod <ahmadadewale800@gmail.com>
2022-12-29 11:57:17 +0000

    Clear unnecessary code

--- app.js
+++ app.js
@@ -13,18 +13,3 @@ navToggle.addEventListener('click', () => {
         navToggle.setAttribute('aria-expanded', false);
     }
 })
-
-
-// const toggle = document.getElementById('toggleMenu');
-// const menuList = document.getElementById('menuList').style;
-
-// menuList.display = 'none';
-
-// toggle.addEventListener('click', () => {
-//     if(!menuList.display){
-//         menuList.display = 'none'
-//     }
-//     else {
-//         menuList.display = ''
-//     }
-// })
